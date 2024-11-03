95e9a5255c007fd29913dc31c4dbd97115b363a5
KShervington <kyle.shervington@gmail.com>
2024-11-02 21:20:45 -0400

    able to get data from db and perform averaging on specific value

--- src/langchain/get_llm_assessment.py
+++ src/langchain/get_llm_assessment.py
@@ -7,12 +7,38 @@ import os
 # Load the environment variables from the .env file
 load_dotenv()
 
+DATABASE_NAME = "test"
+COLLECTION_NAME = "telemetries"
 
-def retrieve_segment_data(segment):
+def retrieve_segment_data():
     # Connect to database
     client = MongoClient(os.getenv("MONGODB_CONNECTION_URI"))
 
-    print('Connected to database from chain!')
+    db = client[DATABASE_NAME]
+    collection = db[COLLECTION_NAME]
+
+    # Get maximum segment from data
+    max_segment_doc  = collection.find().sort("lapInfo.segment", -1).limit(1)
+    max_segment_num = max_segment_doc[0]["lapInfo"]["segment"]
+
+    for seg_num in range(1,max_segment_num+1):
+        # Retrieve data based on segment
+        data = pd.DataFrame(list(collection.find({"lapInfo.segment": seg_num})))
+
+        data_len = len(data)
+
+        sub_segments = 10
+        # Number of values to be averaged within each subsegment
+        window_size = int(data_len / sub_segments)+1
+
+        # Get array of average speeds caclculated for each eub segment
+        avg_speed_array = data['speed'].rolling(window=window_size).mean()[window_size-1::window_size].reset_index(drop=True)
+
+        # Convert averaged data into a string for prompt formatting
+        avg_speed_str = ', '.join([f"{val:.2f}" for val in avg_speed_array])
+
+        print(f'Retrieved {data_len} documents for segment {seg_num}')
+        print(f'Average speeds: {avg_speed_str}\n')
 
     # Close the connection after use
     client.close()
@@ -21,7 +47,7 @@ def retrieve_segment_data(segment):
     
 
 def main():
-    retrieve_segment_data(1)
+    retrieve_segment_data()
 
 
 if __name__ == "__main__":
